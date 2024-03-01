f75010eccb0d5a989d676e360c3ac33be4d48d72
Showvike Mondal Ovi <showvike@gmail.com>
2024-03-01 19:08:01 +0600

    lesson 5 done

---
+++ lesson_5/out/script.js
@@ -0,0 +1,11 @@
+"use strict";
+let a;
+a = "any";
+a = 12;
+let b = [];
+b.push("any", 12);
+let c;
+c = {
+    name: "any",
+    age: 12,
+};
---
+++ lesson_5/src/script.ts
@@ -0,0 +1,18 @@
+let a: any;
+
+a = "any";
+a = 12;
+
+let b: any[] = [];
+
+b.push("any", 12);
+
+let c: {
+  name: any;
+  age: any;
+};
+
+c = {
+  name: "any",
+  age: 12,
+};
