3bacbdc69b8d9273aab5bfd9b6d820fa45195123
Showvike Mondal Ovi <showvike@gmail.com>
2024-03-02 22:46:56 +0600

    lesson 10 done

---
+++ lesson_10/out/script.js
@@ -0,0 +1,34 @@
+"use strict";
+/* class Person {
+  private name: string;
+  public age: number;
+  readonly sex: string;
+
+  constructor(name: string, age: number, sex: string) {
+    this.name = name;
+    this.age = age;
+    this.sex = sex;
+  }
+
+  printPerson() {
+    console.log(`name: ${this.name} age: ${this.age} sex: ${this.sex}`);
+  }
+} */
+class Person {
+    constructor(name, age, sex) {
+        this.name = name;
+        this.age = age;
+        this.sex = sex;
+    }
+    printPerson() {
+        console.log(`name: ${this.name} age: ${this.age} sex: ${this.sex}`);
+    }
+}
+const ovi = new Person("ovi", 27, "male");
+const showvike = new Person("showvike", 27, "male");
+ovi.name = "yo";
+ovi.age = 26;
+ovi.sex = "n/a";
+console.log(ovi.name);
+console.log(ovi.age);
+console.log(ovi.sex);
